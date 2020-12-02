a98195a2e0c183b60915e202a55947415f88a04f
Amin Zagallai <amin.zagallai@gmail.com>
2020-12-02 01:19:32 -0500

    adding login, logout, me, register routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,24 @@
-import { validate, ValidationError } from 'class-validator'
+import { isEmpty, validate } from 'class-validator'
 import { Request, Response, Router } from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
+
 import { User } from '../entities/User'
+import auth from '../middleware/auth'
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body
 
   try {
     let errors: any = {}
+    if (isEmpty(email)) errors.email = 'Email must not be empty'
+    if (isEmpty(username)) errors.username = 'Username must not be empty'
+    if (isEmpty(password)) errors.password = 'Password must not be empty'
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors)
+    }
+
     const emailUser = await User.findOne({ email })
     const usernameUser = await User.findOne({ username })
     if (emailUser) errors.email = 'Email is already exists'
@@ -23,11 +35,70 @@ const register = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error)
     return res.status(500).json(error)
-    //{ error: 'An error was encountered' }
   }
+}
+
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  let errors: any = {}
+  if (isEmpty(username)) errors.username = 'Username must not be empty'
+  if (isEmpty(password)) errors.password = 'Password must not be empty'
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors)
+  }
+
+  try {
+    const user = await User.findOne({ username })
+    if (!user) return res.status(404).json({ error: 'User is not found' })
+
+    const passwordMatched = await bcrypt.compare(password, user.password)
+    if (!passwordMatched)
+      return res.status(404).json({ Password: 'Password is incorrect' })
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET)
+
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 3600,
+        path: '/',
+      })
+    )
+
+    return res.json(user)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+}
+
+const me = (_: Request, res: Response) => {
+  return res.json(res.locals.user)
+}
+
+const logout = async (req: Request, res: Response) => {
+  res.set(
+    'Set-Cookie',
+    cookie.serialize('token', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      expires: new Date(0),
+      path: '/',
+    })
+  )
+
+  return res.json({ sucess: true })
 }
 
 const router = Router()
 router.post('/register', register)
+router.post('/login', login)
+router.get('/me', auth, me)
+router.get('/logout', auth, logout)
 
-export default register
+export default router
--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,22 @@ import express from 'express'
 import morgan from 'morgan'
 import authRoutes from './routes/auth'
 import trim from './middleware/trim'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
 
 const app = express()
-
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim)
+app.use(cookieParser())
 
 app.get('/', (_, res) => res.send('Hello World!'))
 app.use('/api/auth', authRoutes)
 
-app.listen(5000, async () => {
-  console.log('Server is running on 5000')
+app.listen(process.env.PORT, async () => {
+  console.log(`Server is running on ${process.env.PORT}`)
   try {
     await createConnection()
     console.log('Database connected!')
